dd4d284b2b83eb39f0bfd045ab7847104971b28a
thienviva <thienviva7761311@gmail.com>
2021-12-27 03:12:27 +0700

    [ThienHN] Layout Client

---
+++ WebMarket/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebMarket.Models;
+
+namespace WebMarket.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly MarketContext _context;
+
+        public ProductController(MarketContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProductId == id);
+            if(product == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
